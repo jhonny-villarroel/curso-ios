c4e3191fc009014a57a5411df46e7efa1c2b3420
Jhonny Villarroel <jhonny.villarroel@jalasoft.com>
2015-04-28 19:05:48 -0400

    add code Base resapi

---
+++ Swift/newPod/ConnectionRestaurant.swift
@@ -0,0 +1,13 @@
+//
+//  ConnectionRestaurant.swift
+//  newPod
+//
+//  Created by test on 4/28/15.
+//  Copyright (c) 2015 jvm corp. All rights reserved.
+//
+
+import UIKit
+
+class ConnectionRestaurant: NSObject {
+   
+}
---
+++ Swift/newPod/Restaurant.swift
@@ -0,0 +1,28 @@
+//
+//  Restaurant.swift
+//  newPod
+//
+//  Created by test on 4/28/15.
+//  Copyright (c) 2015 jvm corp. All rights reserved.
+//
+
+import UIKit
+import ObjectMapper
+class Restaurant: Mappable {
+    var name:String?
+    var latitud:String!
+    var longtitud:String!
+    //init(){}
+    
+    required init?(_ map: Map) {
+        mapping(map)
+    }
+
+    // Mappable
+    func mapping(map: Map) {
+        name    <- map["name"]
+        latitud         <- map["latitud"]
+        longtitud      <- map["logititud"]
+        
+    }
+}
